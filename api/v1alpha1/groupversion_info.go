757fbd4ec53eb4001cd9e8d571afff68d2a44c38
khandelwalsahil <shkshk.61@gmail.com>
2024-05-20 16:41:34 +0530

    added status

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the api v1alpha1 API group
 // +kubebuilder:object:generate=true
-// +groupName=api.my.domain
+// +groupName=api.sahil.com
 package v1alpha1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "api.my.domain", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "api.sahil.com", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
